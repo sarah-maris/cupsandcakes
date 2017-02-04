0b5873d8e052a10c66b13e3c54b3ccc92a9df397
Sarah Maris <sarah@sarahmaris.com>
2017-02-04 15:52:48 -0500

    Fix content and styled for testimonials page

--- comments.php
+++ comments.php
@@ -1,84 +1,80 @@
 <?php
-// Do not delete these lines
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die (theme_locals("please_do_not"));
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ *
+ * Revised by Sarah Maris for cupsandcakes.com
+ * Updated: 02-04-17
+ */
 
-	if ( post_password_required() ) { ?>
-	<?php echo '<p class="nocomments">' . theme_locals("password") . '</p>'; ?>
-	<?php
-		return;
-	}
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+  return;
+}
 ?>
-<!-- BEGIN Comments -->	
-	<?php if ( have_comments() ) : ?>
-	<div id="comments" class="comment-holder">
-		<h3 class="comments-h cups-comments">What our customers are saying about us</h3>
-		<div class="pagination">
-			<?php paginate_comments_links('prev_text=Prev&next_text=Next'); ?> 
-		</div>
-		<ol class="comment-list cups-list clearfix">
-			<?php wp_list_comments('type=all&callback=mytheme_comment'); ?>
-		</ol>
-		<div class="pagination">
-			<?php paginate_comments_links('prev_text=Prev&next_text=Next'); ?> 
-		</div>
-	</div>
-	<?php else : // this is displayed if there are no comments so far ?>
 
-	<?php if ( comments_open() ) : ?>
-			<!-- If comments are open, but there are no comments. -->
-		<?php echo '<p class="nocomments">' . theme_locals("no_comments_yet") . '</p>'; ?>
-		<?php else : // comments are closed ?>
-			<!-- If comments are closed. -->
-		<?php echo '<p class="nocomments">' . theme_locals("comments_are_closed") . '</p>'; ?>
+<div id="comments" class="comments-area">
 
-		<?php endif; ?>
-	
-	<?php endif; ?>
-	
+  <?php
 
-	<?php if ( comments_open() ) : ?>
+  if ( have_comments() ) : ?>
+    <h2 class="comments-title">
+      <?php
+        $comments_number = get_comments_number();
+        if ( '1' === $comments_number ) {
 
-	<div id="respond" class="cups-comments">
+          printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'twentyseventeen' ), get_the_title() );
+        } else {
+          printf(
 
-	<h3>What do you say?</h3>
+            _nx(
+              '%1$s Reply to &ldquo;%2$s&rdquo;',
+              '%1$s Replies to &ldquo;%2$s&rdquo;',
+              $comments_number,
+              'comments title',
+              'twentyseventeen'
+            ),
+            number_format_i18n( $comments_number ),
+            get_the_title()
+          );
+        }
+      ?>
+    </h2>
 
-	<div class="cancel-comment-reply">
-		<small><?php cancel_comment_reply_link(); ?></small>
-	</div>
+    <ol class="comment-list">
+      <?php
+        wp_list_comments( array(
+          'avatar_size' => 100,
+          'style'       => 'ol',
+          'short_ping'  => true,
+          'callback' => cups_comments,
+          'reply_text'  => twentyseventeen_get_svg( array( 'icon' => 'mail-reply' ) ) . __( 'Reply', 'twentyseventeen' ),
+        ) );
+      ?>
+    </ol>
 
-	<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-	<p><?php echo theme_locals("you_must_be"); ?> <a href="<?php echo wp_login_url( get_permalink() ); ?>"><?php echo theme_locals("logged_in"); ?></a> <?php echo theme_locals("post_a_comment"); ?></p>
-	<?php else : ?>
+    <?php the_comments_pagination( array(
+      'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'twentyseventeen' ) . '</span>',
+      'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+    ) );
 
-	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+  endif; // Check for have_comments().
 
-	<?php if ( is_user_logged_in() ) : ?>
+  // If comments are closed and there are comments, let's leave a little note, shall we?
+  if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-	<p><?php echo theme_locals("logged_in_as"); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php echo theme_locals("log_account"); ?>"><?php echo theme_locals("log_out"); ?></a></p>
+    <p class="no-comments"><?php _e( 'Comments are closed.', 'twentyseventeen' ); ?></p>
+  <?php
+  endif;
 
-	<?php else : ?>
+  comment_form();
+  ?>
 
-	<p class="field cups-list"><input type="text" name="author" id="author" value="Name" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> /></p>
-
-	<p class="field cups-list"><input type="text" name="email" id="email" value="Email (will not be published)" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> /></p>
-
-	<?php endif; ?>
-
-	<!-- <p>You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: <code><?php echo allowed_tags(); ?></code></small></p> -->
-
-	<p><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4" onfocus="if(this.value=='<?php echo theme_locals("your_comment"); ?>'){this.value=''}" onblur="if(this.value==''){this.value='<?php echo theme_locals("your_comment"); ?>'}"><?php echo theme_locals("your_comment"); ?></textarea></p>
-
-	<p class="comment_submit"><input name="submit" type="submit" class="btn btn-primary" id="submit" tabindex="5" value="<?php echo theme_locals("submit_comment"); ?>" />
-		<?php comment_id_fields(); ?>
-	</p>
-	<?php do_action('comment_form', $post->ID); ?>
-
-	</form>
-
-	<?php endif; // If registration required and not logged in ?>
-	</div>
-
-<!-- END Comments -->
-
-<?php endif; ?>
+</div><!-- #comments -->
--- functions.php
+++ functions.php
@@ -1,6 +1,4 @@
 <?php
-
-<?php
 /**
  * Additional Functions
  *
@@ -15,6 +13,8 @@
  *
  *  2.0 - Create Class Taxonomy for pages
  *
+ *  3.0 - Customize comment output
+ *
  */
 
 
@@ -28,7 +28,7 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
    wp_enqueue_style('bootstrap-css',get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.min.css');
    wp_enqueue_script('bootstrap-js',get_stylesheet_directory_uri().'/bootstrap/js/bootstrap.min.js');
- 
+
 }
 
 
@@ -39,27 +39,77 @@ function enqueue_parent_styles() {
 
  // create taxonomy to add CSS class to page/section
  function create_class_taxonomy() {
- 	// Add new taxonomy, make it hierarchical (like categories)
- 	$labels = array(
- 		'name'              => _x( 'Page Class', 'taxonomy general name', 'textdomain' ),
- 		'singular_name'     => _x( 'Page Class', 'taxonomy singular name', 'textdomain' ),
- 		'search_items'      => __( 'Search Page Classes', 'textdomain' ),
- 		'all_items'         => __( 'All Page Classes', 'textdomain' ),
+  // Add new taxonomy, make it hierarchical (like categories)
+  $labels = array(
+    'name'              => _x( 'Page Class', 'taxonomy general name', 'textdomain' ),
+    'singular_name'     => _x( 'Page Class', 'taxonomy singular name', 'textdomain' ),
+    'search_items'      => __( 'Search Page Classes', 'textdomain' ),
+    'all_items'         => __( 'All Page Classes', 'textdomain' ),
 
- 		'edit_item'         => __( 'Edit Page Class', 'textdomain' ),
- 		'update_item'       => __( 'Update Page Class', 'textdomain' ),
- 		'add_new_item'      => __( 'Add New Page Class', 'textdomain' ),
- 		'new_item_name'     => __( 'New Page Class Name', 'textdomain' ),
- 		'menu_name'         => __( 'Page Class', 'textdomain' ),
- 	);
+    'edit_item'         => __( 'Edit Page Class', 'textdomain' ),
+    'update_item'       => __( 'Update Page Class', 'textdomain' ),
+    'add_new_item'      => __( 'Add New Page Class', 'textdomain' ),
+    'new_item_name'     => __( 'New Page Class Name', 'textdomain' ),
+    'menu_name'         => __( 'Page Class', 'textdomain' ),
+  );
 
- 	$args = array(
- 		'hierarchical'      => false,
- 		'labels'            => $labels,
- 		'show_ui'           => true,
- 		'show_admin_column' => true,
- 		'query_var'         => true,
- 	);
+  $args = array(
+    'hierarchical'      => false,
+    'labels'            => $labels,
+    'show_ui'           => true,
+    'show_admin_column' => true,
+    'query_var'         => true,
+  );
 
- 	register_taxonomy( 'page_classes', 'page', $args );
+  register_taxonomy( 'page_classes', 'page', $args );
+}
+
+/*  3.0 CUSTOM COMMENT OUTPUT
+ * --------------------------------------------------*/
+// source:  https://saltnpixels.com/customizing-the-wp-comments-section-using-callback/
+
+function cups_comments($comment, $args, $depth){
+   //checks if were using a div or ol|ul for our output
+   $tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
+?>
+      <<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $args['has_children'] ? 'parent' : '', $comment ); ?>>
+         <article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
+            <footer class="comment-meta">
+               <div class="comment-author vcard">
+                  <?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+                  <?php printf( __( '%s <span class="says">says:</span>' ), sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) ) ); ?>
+               </div><!-- .comment-author -->
+
+               <div class="comment-metadata">
+
+                     <time datetime="<?php comment_time( 'c' ); ?>">
+                        <?php
+                           /* translators: 1: comment date, 2: comment time */
+                           printf( __( '%1$s' ), get_comment_date( '', $comment )); // CUPS CHANGE - REMOVE COMMENT TIME
+                        ?>
+                     </time>
+
+                  <?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
+               </div><!-- .comment-metadata -->
+
+               <?php if ( '0' == $comment->comment_approved ) : ?>
+               <p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
+               <?php endif; ?>
+            </footer><!-- .comment-meta -->
+
+            <div class="comment-content">
+               <?php comment_text(); ?>
+            </div><!-- .comment-content -->
+
+            <?php
+            comment_reply_link( array_merge( $args, array(
+               'add_below' => 'div-comment',
+               'depth'     => $depth,
+               'max_depth' => $args['max_depth'],
+               'before'    => '<div class="reply">',
+               'after'     => '</div>'
+            ) ) );
+            ?>
+         </article><!-- .comment-body -->
+         <?php
 }
