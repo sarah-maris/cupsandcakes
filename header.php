72a1a4605a15efb16851f70de2ff68bd3b88bd85
Sarah Maris <sarah@sarahmaris.com>
2017-02-05 18:34:45 -0500

    Clean up templates

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * Added cupscakes class
- * 
+ *
  * Revised 1-12-17
  */
 
@@ -21,48 +21,48 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site cupscakes">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
-				<?php if ( has_nav_menu( 'social' ) ) : ?>
-					<div class="social-navigation header-social" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
-						?>
-					</div><!-- .social-navigation -->
-				<?php endif;?>
+  <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
+        <?php if ( has_nav_menu( 'social' ) ) : ?>
+          <div class="social-navigation header-social" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+            <?php
+              wp_nav_menu( array(
+                'theme_location' => 'social',
+                'menu_class'     => 'social-links-menu',
+                'depth'          => 1,
+                'link_before'    => '<span class="screen-reader-text">',
+                'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+              ) );
+            ?>
+          </div><!-- .social-navigation -->
+        <?php endif;?>
 <div class="cupscakes-hours">
-	<!--Mon-Wed 6:00AM-5:00PM<br>
-	Thurs–Sat: 6:00AM-10:00PM<br>-->
-	Mon-Sat 6:00AM-5:00PM<br>
-	Sunday: 6:00AM–2:00PM
+  <!--Mon-Wed 6:00AM-5:00PM<br>
+  Thurs–Sat: 6:00AM-10:00PM<br>-->
+  Mon-Sat 6:00AM-5:00PM<br>
+  Sunday: 6:00AM–2:00PM
 </div>
-	<header id="masthead" class="site-header" role="banner">
+  <header id="masthead" class="site-header" role="banner">
 
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
+    <?php get_template_part( 'template-parts/header/header', 'image' ); ?>
 
-		<?php if ( has_nav_menu( 'top' ) ) : ?>
-			<div class="navigation-top">
-				<div class="wrap">
-					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
-				</div><!-- .wrap -->
-			</div><!-- .navigation-top -->
-		<?php endif; ?>
+    <?php if ( has_nav_menu( 'top' ) ) : ?>
+      <div class="navigation-top">
+        <div class="wrap">
+          <?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+        </div><!-- .wrap -->
+      </div><!-- .navigation-top -->
+    <?php endif; ?>
 
-	</header><!-- #masthead -->
+  </header><!-- #masthead -->
 
-	<?php
-	// If a regular post or page, and not the front page, show the featured image.
-	if ( has_post_thumbnail() && ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) ) :
-		echo '<div class="single-featured-image-header">';
-		the_post_thumbnail( 'twentyseventeen-featured-image' );
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
+  <?php
+  // If a regular post or page, and not the front page, show the featured image.
+  if ( has_post_thumbnail() && ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) ) :
+    echo '<div class="single-featured-image-header">';
+    the_post_thumbnail( 'twentyseventeen-featured-image' );
+    echo '</div><!-- .single-featured-image-header -->';
+  endif;
+  ?>
 
-	<div class="site-content-contain">
-		<div id="content" class="site-content">
+  <div class="site-content-contain">
+    <div id="content" class="site-content">
--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -2,14 +2,10 @@
 /**
  * Displays footer site info
  *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
+ * Revised by Sarah Maris for CupsandCakes.com
+ * Updated: 01-19-17
  *
- * Revised by Sarah Maris for ensureye.com
- * Updated: 11-19-16
- */
+ **/
 
 ?>
 <div class="site-info">
