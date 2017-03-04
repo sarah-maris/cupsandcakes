54bfa26771085f6f463ed70fc22cecd414aaa101
Sarah Maris <sarah@sarahmaris.com>
2017-03-03 21:37:23 -0800

    Add phone number to header and email to footer

---
+++ template-parts/header/site-branding.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * Revised by Sarah Maris for CupsandCakes.com
+ * Updated: 03-03-17
+ *
+ **/
+
+?>
+<div class="site-branding">
+	<div class="wrap">
+
+		<?php the_custom_logo(); ?>
+
+		<div class="site-branding-text">
+			<?php if ( is_front_page() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php endif; ?>
+
+			<?php $description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; ?>
+          <br>
+          122 E. River Rd, Rumson NJ<br>
+          <a class="contact-link" href="tel:(732)%20842-8080" value="+17328428080" target="_blank">732-842-8080</a></p>
+				<?php endif; ?>
+		</div><!-- .site-branding-text -->
+
+		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
+	<?php endif; ?>
+
+	</div><!-- .wrap -->
+</div><!-- .site-branding -->
